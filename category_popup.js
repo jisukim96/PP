1fc3ea313619277aaccb74f6f5ca45959841526d
jisu kim <yyjisu@gmail.com>
2023-01-19 07:48:52 +0900

    0118-home

---
+++ category_popup.js
@@ -0,0 +1,30 @@
+
+/*var popupopen = document.getElementsByClassName("fa-sharp fa-solid fa-folder-tree");
+var category = document.getElementsByClassName("popup-back");
+var popupclose = document.getElementsByClassName("popup-button-close"); 
+
+addEventListener("click",showpopup);
+
+function showpopup(){
+    category.style.display = "block";
+}
+
+popupclose.onclick = function(){
+    category.style.display = "none";
+}*/
+
+/*
+$(document).ready(function(){
+    $('.popup-button-close').click(function(){
+        $('popup-back').hide();
+    });
+    $('.fa-sharp fa-solid fa-folder-tree').click(function(){
+        $('popup-back').show();
+
+    });
+
+});*/
+
+function showpopup(){
+    .popup-back
+}
